db2943c98c285a900b02d0bf3489ec4d9bfcd93e
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-06-01 17:27:12 -0700

    file refactor

--- src/app/api/user/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextRequest} from 'next/server';
-
-export async function GET(req: NextRequest) {
-  console.log("get request")
-  return new Response("200 OK")
-}
-export async function POST() {
-  console.log("post request")
-}
